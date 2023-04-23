d6fd0ed41084a8aa2785b3b663911883431ee610
karolukasik <karo.lukasik@gmail.com>
2023-04-23 19:20:15 +0200

    Many To Many unidirectional lesson

---
+++ become-spring-boot-dev/spring-databases/database-relationships/src/main/java/io/datajek/databaserelationships/manytomany/Category.java
@@ -0,0 +1,26 @@
+package io.datajek.databaserelationships.manytomany;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String name;
+}
--- become-spring-boot-dev/spring-databases/database-relationships/src/main/java/io/datajek/databaserelationships/manytomany/Tournament.java
+++ become-spring-boot-dev/spring-databases/database-relationships/src/main/java/io/datajek/databaserelationships/manytomany/Tournament.java
@@ -6,11 +6,16 @@ import static javax.persistence.GenerationType.IDENTITY;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -42,6 +47,11 @@ public class Tournament {
     @JoinColumn(name = "tournament_id")
     private List<Registration> registrations = new ArrayList<>();
 
+    @ManyToMany(cascade = { CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
+    @JoinTable(name = "tournament_categories", joinColumns = @JoinColumn(name = "tournament_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @JsonIgnoreProperties("tournaments")
+    private List<Category> playingCategories = new ArrayList<>();
+
     public void addRegistration(Registration reg) {
         registrations.add(reg);
     }
@@ -51,4 +61,13 @@ public class Tournament {
             registrations.remove(reg);
         }
     }
+
+    public void addCategory(Category category) {
+        playingCategories.add(category);
+    }
+
+	public void removeCategory(Category category) {
+		if (playingCategories != null)
+			playingCategories.remove(category);
+	}
 }
